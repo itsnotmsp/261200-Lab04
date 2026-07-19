f86d1bc3d86e4bbd47547699a4144c8d9ca8c15d
680615527MinSwanPyei <minswanpyae10@gmail.com>
2026-07-19 20:27:06 +0700

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public static void main(String[] args) {
+        // Create and test student records
+        HealthRecord student1 = new HealthRecord(120);
+        HealthRecord student2 = new HealthRecord(55);
+        HealthRecord student3 = new HealthRecord(280);
 
-public class Main {
+        // Display individual records
+        System.out.println(student1.getDetails()); // Expected: 120
+        System.out.println(student2.getDetails()); // Expected: 55
+        System.out.println(student3.getDetails()); // Expected: 165 (default, as 280 is invalid)
+
+        // Display class statistics
+        System.out.println(HealthRecord.getClassDetails()); // Expected: tallest=120, shortest=55
 }
